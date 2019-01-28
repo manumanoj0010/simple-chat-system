03ffa31e163e3b715bb4686efd324c6dcd4b24d2
manumanoj0010 <31968193+manumanoj0010@users.noreply.github.com>
2019-01-28 12:20:27 +0530

    update chat.sql

--- chat.sql
+++ chat.sql
@@ -2,10 +2,10 @@
 -- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 09, 2018 at 10:54 PM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Host: 127.0.0.1
+-- Generation Time: Jan 28, 2019 at 07:49 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,14 +28,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `chat`
 --
 
-DROP TABLE IF EXISTS `chat`;
-CREATE TABLE IF NOT EXISTS `chat` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `msg` varchar(255) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=109 DEFAULT CHARSET=latin1;
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `chat`
@@ -43,7 +41,63 @@ CREATE TABLE IF NOT EXISTS `chat` (
 
 INSERT INTO `chat` (`id`, `name`, `msg`, `date`) VALUES
 (108, 'jane smith', 'sample response', '2018-11-09 22:53:05'),
-(107, 'john doe', 'sample message', '2018-11-09 22:52:47');
+(107, 'john doe', 'sample message', '2018-11-09 22:52:47'),
+(109, 'manoj', 'hey there', '2019-01-13 16:54:48'),
+(110, 'nithin', 'hey whatsapp', '2019-01-13 16:55:11'),
+(122, 'nithin', 'asdfsdf', '2019-01-13 19:13:02'),
+(123, 'nithin', 'asdfsdf', '2019-01-13 19:15:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(1, 'manumanoj0010', 'abcd'),
+(2, 'nithin', 'abcd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
